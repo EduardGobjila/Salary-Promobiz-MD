393ed5956822e7c4a72d9d4fbce681e8bc131f11
Eduard Gobjila <edikgobjila01@gmail.com>
2023-02-16 12:22:20 +0000

    edited the form for sign in + added first name last name + added ontroller for salaries

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,5 +2,10 @@ class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :validatable
+         :recoverable, :rememberable, :validatable,
+         :authentication_keys => [:first_name, :last_name, :email]
+
+  validates :email, uniqueness: true
+  validates :first_name, presence: true
+  validates :last_name, presence: true
 end
