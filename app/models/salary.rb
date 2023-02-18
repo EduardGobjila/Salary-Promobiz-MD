b509363766cc7538fd84c5a10014f9d85fd4df08
Eduard Gobjila <edikgobjila01@gmail.com>
2023-02-18 12:57:15 +0000

    fixed the bug with the validations

--- app/models/salary.rb
+++ app/models/salary.rb
@@ -1,6 +1,6 @@
 class Salary < ApplicationRecord
   belongs_to :user
-  validates :month presence: true
+  validates :month, presence: true
 
   MONTHS = ['January', 'February', 'March', 'April', 'May', 'June',
                  'July', 'Aug', 'September', 'Octomber', 'November', 'December']
