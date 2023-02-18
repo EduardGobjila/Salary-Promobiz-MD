7934acc7bc05721a2aea18263f413f3647ec4aea
Eduard Gobjila <edikgobjila01@gmail.com>
2023-02-18 12:56:52 +0000

    added a validation for the salary

--- app/models/salary.rb
+++ app/models/salary.rb
@@ -1,5 +1,6 @@
 class Salary < ApplicationRecord
   belongs_to :user
+  validates :month presence: true
 
   MONTHS = ['January', 'February', 'March', 'April', 'May', 'June',
                  'July', 'Aug', 'September', 'Octomber', 'November', 'December']
