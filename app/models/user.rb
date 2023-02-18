bee89852aaf3c76204e13b9704160f14df1fb3c2
Eduard Gobjila <edikgobjila01@gmail.com>
2023-02-18 16:33:03 +0000

    fixing the prevompiling in production

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,12 +2,12 @@ class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :validatable,
-        :authentication_keys => [:email, :first_name, :last_name]
+         :recoverable, :rememberable, :validatable
+        # :authentication_keys => [:email, :first_name, :last_name]
 
-  validates :email, uniqueness: true
-  validates :first_name, presence: true
-  validates :last_name, presence: true
+  # validates :email, uniqueness: true
+  # validates :first_name, presence: true
+  # validates :last_name, presence: true
 
   has_many :salaries
 end
