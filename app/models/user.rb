2d9d9c218b9bcf5d9f659b1c99deec7541dcf4f6
Eduard Gobjila <edikgobjila01@gmail.com>
2023-02-18 14:03:57 +0000

    trying to fix when sign in without first name and last name just email and pasword

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,8 +2,8 @@ class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :validatable
-        # :authentication_keys => [:email, :first_name, :last_name]
+         :recoverable, :rememberable, :validatable,
+        :authentication_keys => [:email, :first_name, :last_name]
 
   validates :email, uniqueness: true
   validates :first_name, presence: true
