04bc9b29897b2e776071b6482ad16c60130d6173
Eduard Gobjila <edikgobjila01@gmail.com>
2023-02-18 18:33:34 +0000

    fixed the login with permitted content

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,11 +3,11 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
-        # :authentication_keys => [:email, :first_name, :last_name]
 
-  # validates :email, uniqueness: true
-  # validates :first_name, presence: true
-  # validates :last_name, presence: true
+
+  validates :email, uniqueness: true
+  validates :first_name, presence: true
+  validates :last_name, presence: true
 
   has_many :salaries
 end
